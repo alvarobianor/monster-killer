932b00402be4e186a8124dff254f308f7e7acc09
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-06-22 17:00:40 -0300

    btn attack and especial-attack updated

--- app.js
+++ app.js
@@ -16,6 +16,29 @@ new Vue({
       this.playerLife = 100;
       this.monsterLife = 100;
     },
+    attack() {
+      this.monsterLife = Math.max(
+        this.monsterLife - this.hurt(5, 10, false),
+        0
+      );
+      this.playerLife = Math.max(this.playerLife - this.hurt(7, 12, false), 0);
+    },
+    especialAttack() {
+      this.monsterLife = Math.max(this.monsterLife - this.hurt(5, 10, true), 0);
+      this.playerLife = Math.max(this.playerLife - this.hurt(7, 12, false), 0);
+    },
+    hurt(min, max, isEspecial) {
+      const especial = isEspecial ? 5 : 0;
+
+      return this.randomValue(min + especial, max + especial);
+    },
+    randomValue(min, max) {
+      return (Math.random() * (max - min) + min).toFixed(0);
+    },
   },
-  watch: {},
+  watch: {
+    hasResult(result) {
+      if (result) this.running = false;
+    },
+  },
 });
