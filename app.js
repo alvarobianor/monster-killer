7fcd2870a48b2a4a74afa5600d0e4538450ce81a
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-06-22 17:31:17 -0300

    add change sword and substitution the 'especial-attack'

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
     running: false,
     playerLife: 100,
     monsterLife: 100,
+    sword: "basic",
   },
   computed: {
     hasResult() {
@@ -17,23 +18,28 @@ new Vue({
       this.monsterLife = 100;
     },
     attack() {
-      this.monsterLife = Math.max(
-        this.monsterLife - this.hurt(5, 10, false),
-        0
-      );
+      let min = 5,
+        max = 10;
+      if (this.sword === "up") {
+        min = 10;
+        max = 15;
+      }
+      this.monsterLife = Math.max(this.monsterLife - this.hurt(min, max), 0);
       this.playerLife = Math.max(this.playerLife - this.hurt(7, 12, false), 0);
     },
-    especialAttack() {
-      this.monsterLife = Math.max(this.monsterLife - this.hurt(5, 10, true), 0);
-      this.playerLife = Math.max(this.playerLife - this.hurt(7, 12, false), 0);
-    },
-    hurt(min, max, isEspecial) {
-      const especial = isEspecial ? 5 : 0;
-
-      return this.randomValue(min + especial, max + especial);
+    // especialAttack() {
+    //   this.monsterLife = Math.max(this.monsterLife - this.hurt(5, 10, true), 0);
+    //   this.playerLife = Math.max(this.playerLife - this.hurt(7, 12, false), 0);
+    // },
+    hurt(min, max) {
+      return this.randomValue(min, max);
     },
     randomValue(min, max) {
       return (Math.random() * (max - min) + min).toFixed(0);
+    },
+    changeSword(value) {
+      this.sword = value;
+      console.log(this.sword);
     },
   },
   watch: {
